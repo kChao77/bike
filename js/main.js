3ee18c6a20570311168350060adad55c1908ef91
Askarov <101247445+kChao77@users.noreply.github.com>
2022-06-28 08:46:12 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('.bike__slider, .slider__items').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplaySpeed: 2000
+  });
+
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
+
+});
